6b9325abd4cac888067519fd4b6e884ae953eb08
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-05-29 20:08:42 +0100

    Delete Scripts/POWERSHELL/Networking/Get-IPConfig.ps1

--- Scripts/POWERSHELL/Networking/Get-IPConfig.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "=== IP Configuration ==="
-Write-Host ""
-
-Get-NetIPAddress |
-    Select-Object InterfaceAlias, AddressFamily, IPAddress, PrefixLength |
-    Sort-Object InterfaceAlias |
-    Format-Table -AutoSize
