118287f55fe0412d87f421b4bf50bb32fb3ddc10
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-05-29 16:13:02 +0100

    chore: add CMD's

--- Scripts/CMD/system-summary.cmd
+++ Scripts/CMD/system-summary.cmd
@@ -4,7 +4,11 @@ echo Hostname: %COMPUTERNAME%
 echo User: %USERNAME%
 echo Date: %DATE%
 echo Time: %TIME%
+echo.
 echo Windows Version:
 ver
+echo.
+echo Uptime:
+net statistics workstation | findstr /i "since"
 echo.
 pause
