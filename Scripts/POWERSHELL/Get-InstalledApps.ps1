166e408b468982d0aa1a9fbd254a9e8c6338e4c3
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-05-29 16:14:17 +0100

    added 32 bit registry path and publisher

--- Scripts/POWERSHELL/Get-InstalledApps.ps1
+++ Scripts/POWERSHELL/Get-InstalledApps.ps1
@@ -1,7 +1,14 @@
+$paths = @(
+    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*",
+    "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+)
+
 Write-Host "Installed Applications"
 Write-Host "----------------------"
 
-Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
-    Select-Object DisplayName, DisplayVersion |
+$paths |
+    ForEach-Object { Get-ItemProperty $_ } |
     Where-Object { $_.DisplayName } |
-    Sort-Object DisplayName
+    Select-Object DisplayName, DisplayVersion, Publisher |
+    Sort-Object DisplayName |
+    Format-Table -AutoSize
